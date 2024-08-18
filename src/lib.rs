40a0592f437042d2b540c0dfc333b807a5999640
beeb <703631+beeb@users.noreply.github.com>
2024-08-18 14:58:23 +0200

    refactor: lib error handling

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@ pub use crate::{commands::Subcommands, errors::SoldeerError};
 use cliclack::{
     intro,
     log::{remark, step, success, warning},
-    multi_progress, outro, spinner,
+    multi_progress, outro, outro_cancel,
 };
 use config::{
     add_to_config, get_config_path, read_soldeer_config, remappings_foundry, remove_forge_lib,
@@ -59,127 +59,18 @@ pub async fn run(command: Subcommands) -> Result<(), SoldeerError> {
         Subcommands::Init(init) => {
             intro("🦌 Soldeer Init 🦌")?;
             step("Initialize Foundry project to use Soldeer")?;
-
-            if init.clean {
-                remark("Flag `--clean` was set, removing `lib` dir and submodules")?;
-                remove_forge_lib().await?;
-            }
-
-            let config_path = FOUNDRY_CONFIG_FILE.as_path();
-            let config = read_soldeer_config(Some(config_path))?;
-            success("Done reading config")?;
-            let dependency = get_latest_forge_std().await.map_err(|e| {
-                SoldeerError::DownloadError { dep: "forge-std".to_string(), source: e }
+            init_command(init).await.map_err(|e| {
+                let _ = outro_cancel("An error occurred during initialization");
+                e
             })?;
-            ensure_dependencies_dir()?;
-            let multi = multi_progress(format!("Installing {dependency}"));
-            let progress = Progress::new(&multi, 1);
-            progress.start_all();
-            let lock = install_dependency(&dependency, None, false, progress.clone())
-                .await
-                .map_err(|e| {
-                    multi.error(&e);
-                    e
-                })?;
-            progress.stop_all();
-            multi.stop();
-            add_to_config(&dependency, config_path)?;
-            success("Dependency added to config")?;
-            add_to_lockfile(lock)?;
-            success("Dependency added to lockfile")?;
-            add_to_remappings(dependency, &config, config_path).await?;
-            success("Dependency added to remappings")?;
-            // TODO: add `dependencies` to the .gitignore file if it exists
             outro("Done initializing!")?;
         }
-        Subcommands::Install(install) => {
+        Subcommands::Install(cmd) => {
             intro("🦌 Soldeer Install 🦌")?;
-            let config_path = get_config_path()?;
-            let mut config = read_soldeer_config(Some(&config_path))?;
-            if install.regenerate_remappings {
-                config.remappings_regenerate = true;
-            }
-            if install.recursive_deps {
-                config.recursive_deps = true;
-            }
-            success("Done reading config")?;
-            ensure_dependencies_dir()?;
-            let dependencies: Vec<Dependency> = read_config_deps(Some(&config_path))?;
-            match install.dependency {
-                None => {
-                    let (locks, lockfile_content) = read_lockfile()?;
-                    success("Done reading lockfile")?;
-                    if install.clean {
-                        remark("Flag `--clean` was set, re-installing all dependencies")?;
-                        fs::remove_dir_all(DEPENDENCY_DIR.as_path()).map_err(|e| {
-                            InstallError::IOError { path: DEPENDENCY_DIR.to_path_buf(), source: e }
-                        })?;
-                        ensure_dependencies_dir()?;
-                    }
-                    let multi = multi_progress("Installing dependencies");
-                    let progress = Progress::new(&multi, dependencies.len() as u64);
-                    progress.start_all();
-                    let new_locks = install_dependencies(
-                        &dependencies,
-                        &locks,
-                        config.recursive_deps,
-                        progress.clone(),
-                    )
-                    .await
-                    .map_err(|e| {
-                        multi.error(&e);
-                        e
-                    })?;
-                    progress.stop_all();
-                    multi.stop();
-                    let new_lockfile_content = generate_lockfile_contents(new_locks);
-                    if !locks.is_empty() && new_lockfile_content != lockfile_content {
-                        warning("Warning: the lock file is out of sync with the dependencies. Consider running `soldeer lock` or `soldeer update` to re-generate the lockfile.")?;
-                    } else if locks.is_empty() {
-                        fs::write(LOCK_FILE.as_path(), new_lockfile_content)
-                            .map_err(LockError::IOError)?;
-                    }
-                    update_remappings(&config, &config_path).await?;
-                    success("Updated remappings")?;
-                }
-                Some(dependency) => {
-                    let mut dep = Dependency::from_name_version(
-                        &dependency,
-                        install.remote_url,
-                        install.rev,
-                    )?;
-                    if dependencies
-                        .iter()
-                        .any(|d| d.name() == dep.name() && d.version() == dep.version())
-                    {
-                        outro(format!("{dep} is already installed"))?;
-                        return Ok(());
-                    }
-                    let multi = multi_progress(format!("Installing {dep}"));
-                    let progress = Progress::new(&multi, 1);
-                    progress.start_all();
-                    let lock =
-                        install_dependency(&dep, None, config.recursive_deps, progress.clone())
-                            .await
-                            .map_err(|e| {
-                                multi.error(&e);
-                                e
-                            })?;
-                    progress.stop_all();
-                    multi.stop();
-                    // for GIT deps, we need to add the commit hash before adding them to the
-                    // config.
-                    if let Some(git_dep) = dep.as_git_mut() {
-                        git_dep.rev = Some(lock.checksum.clone());
-                    }
-                    add_to_config(&dep, &config_path)?;
-                    success("Dependency added to config")?;
-                    add_to_lockfile(lock)?;
-                    success("Dependency added to lockfile")?;
-                    add_to_remappings(dep, &config, &config_path).await?;
-                    success("Dependency added to remappings")?;
-                }
-            }
+            install_command(cmd).await.map_err(|e| {
+                let _ = outro_cancel("An error occurred during installation");
+                e
+            })?;
             outro("Done installing!")?;
         }
         Subcommands::Update(update_args) => {
@@ -275,6 +166,112 @@ pub async fn run(command: Subcommands) -> Result<(), SoldeerError> {
         Subcommands::Version(_) => {
             const VERSION: &str = env!("CARGO_PKG_VERSION");
             println!("{}", format!("Current Soldeer {}", VERSION).cyan());
+        }
+    }
+    Ok(())
+}
+
+async fn init_command(cmd: commands::Init) -> Result<(), SoldeerError> {
+    if cmd.clean {
+        remark("Flag `--clean` was set, removing `lib` dir and submodules")?;
+        remove_forge_lib().await?;
+    }
+
+    let config_path = FOUNDRY_CONFIG_FILE.as_path();
+    let config = read_soldeer_config(Some(config_path))?;
+    success("Done reading config")?;
+    let dependency = get_latest_forge_std()
+        .await
+        .map_err(|e| SoldeerError::DownloadError { dep: "forge-std".to_string(), source: e })?;
+    ensure_dependencies_dir()?;
+    let multi = multi_progress(format!("Installing {dependency}"));
+    let progress = Progress::new(&multi, 1);
+    progress.start_all();
+    let lock =
+        install_dependency(&dependency, None, false, progress.clone()).await.map_err(|e| {
+            multi.error(&e);
+            e
+        })?;
+    progress.stop_all();
+    multi.stop();
+    add_to_config(&dependency, config_path)?;
+    success("Dependency added to config")?;
+    add_to_lockfile(lock)?;
+    success("Dependency added to lockfile")?;
+    add_to_remappings(dependency, &config, config_path).await?;
+    success("Dependency added to remappings")?;
+    // TODO: add `dependencies` to the .gitignore file if it exists
+    Ok(())
+}
+
+async fn install_command(cmd: commands::Install) -> Result<(), SoldeerError> {
+    let config_path = get_config_path()?;
+    let mut config = read_soldeer_config(Some(&config_path))?;
+    if cmd.regenerate_remappings {
+        config.remappings_regenerate = true;
+    }
+    if cmd.recursive_deps {
+        config.recursive_deps = true;
+    }
+    success("Done reading config")?;
+    ensure_dependencies_dir()?;
+    let dependencies: Vec<Dependency> = read_config_deps(Some(&config_path))?;
+    match cmd.dependency {
+        None => {
+            let (locks, lockfile_content) = read_lockfile()?;
+            success("Done reading lockfile")?;
+            if cmd.clean {
+                remark("Flag `--clean` was set, re-installing all dependencies")?;
+                fs::remove_dir_all(DEPENDENCY_DIR.as_path()).map_err(|e| {
+                    InstallError::IOError { path: DEPENDENCY_DIR.to_path_buf(), source: e }
+                })?;
+                ensure_dependencies_dir()?;
+            }
+            let multi = multi_progress("Installing dependencies");
+            let progress = Progress::new(&multi, dependencies.len() as u64);
+            progress.start_all();
+            let new_locks = install_dependencies(
+                &dependencies,
+                &locks,
+                config.recursive_deps,
+                progress.clone(),
+            )
+            .await?;
+            progress.stop_all();
+            multi.stop();
+            let new_lockfile_content = generate_lockfile_contents(new_locks);
+            if !locks.is_empty() && new_lockfile_content != lockfile_content {
+                warning("Warning: the lock file is out of sync with the dependencies. Consider running `soldeer lock` or `soldeer update` to re-generate the lockfile.")?;
+            } else if locks.is_empty() {
+                fs::write(LOCK_FILE.as_path(), new_lockfile_content).map_err(LockError::IOError)?;
+            }
+            update_remappings(&config, &config_path).await?;
+            success("Updated remappings")?;
+        }
+        Some(dependency) => {
+            let mut dep = Dependency::from_name_version(&dependency, cmd.remote_url, cmd.rev)?;
+            if dependencies.iter().any(|d| d.name() == dep.name() && d.version() == dep.version()) {
+                outro(format!("{dep} is already installed"))?;
+                return Ok(());
+            }
+            let multi = multi_progress(format!("Installing {dep}"));
+            let progress = Progress::new(&multi, 1);
+            progress.start_all();
+            let lock =
+                install_dependency(&dep, None, config.recursive_deps, progress.clone()).await?;
+            progress.stop_all();
+            multi.stop();
+            // for GIT deps, we need to add the commit hash before adding them to the
+            // config.
+            if let Some(git_dep) = dep.as_git_mut() {
+                git_dep.rev = Some(lock.checksum.clone());
+            }
+            add_to_config(&dep, &config_path)?;
+            success("Dependency added to config")?;
+            add_to_lockfile(lock)?;
+            success("Dependency added to lockfile")?;
+            add_to_remappings(dep, &config, &config_path).await?;
+            success("Dependency added to remappings")?;
         }
     }
     Ok(())

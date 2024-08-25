6903b99b7424e550b8956646f0f13b1148443a88
beeb <703631+beeb@users.noreply.github.com>
2024-08-25 19:39:48 +0200

    refactor: remove yansi

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 use clap::Parser;
 use soldeer::commands::Args;
-use yansi::Paint as _;
 
 fn main() {
     let args = Args::parse();
     match soldeer::run(args.command) {
         Ok(_) => {}
         Err(err) => {
-            eprintln!("{}", err.to_string().red())
+            eprintln!("{err}");
         }
     }
 }

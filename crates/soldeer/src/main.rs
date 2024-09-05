2e93d5ecd3de1b7099a39daf5f4c80ec8e337223
beeb <703631+beeb@users.noreply.github.com>
2024-09-05 16:46:47 +0200

    feat(bin): restore yansi

--- crates/soldeer/src/main.rs
+++ crates/soldeer/src/main.rs
@@ -1,11 +1,11 @@
 //! Soldeer is a package manager for Solidity projects
-use owo_colors::{OwoColorize, Stream::Stderr};
 use soldeer_commands::{commands::Parser as _, run, Args};
+use yansi::Paint as _;
 
 #[tokio::main]
 async fn main() {
     let args = Args::parse();
     if let Err(err) = run(args.command).await {
-        eprintln!("{}", err.to_string().if_supports_color(Stderr, |t| t.red()))
+        eprintln!("{}", err.to_string().red())
     }
 }

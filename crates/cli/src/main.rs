81c2c227f55c89c346f47d1acd8e49c75e58e3bc
xyizko <164354015+xyizko@users.noreply.github.com>
2024-09-26 22:38:09 +0000

    (Ehancement) Add Banner - Display banner with useful information

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -4,8 +4,25 @@ use yansi::Paint as _;
 
 #[tokio::main]
 async fn main() {
+    banner();
     let args = Args::parse();
     if let Err(err) = run(args.command).await {
         eprintln!("{}", err.to_string().red())
     }
+}
+
+// Banner edittable 
+fn banner() {
+    println!(
+        "{}",
+        "
++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    ╔═╗╔═╗╦  ╔╦╗╔═╗╔═╗╦═╗     Solidity Package Manager 
+    ╚═╗║ ║║   ║║║╣ ║╣ ╠╦╝          built in rust
+    ╚═╝╚═╝╩═╝═╩╝╚═╝╚═╝╩╚═    github.com/mario-eth/soldeer   
+         soldeer.xyz              x.com/m4rio_eth
++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+"
+        .bright_cyan()
+    );
 }

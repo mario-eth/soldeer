7ee8fb35a3f7765b9c5dc446e5ffe5dd287ad6a1
beeb <703631+beeb@users.noreply.github.com>
2024-10-03 11:04:19 +0200

    fix(cli): respect environment and tty preference for color

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,9 +1,17 @@
 //! Soldeer is a package manager for Solidity projects
 use soldeer_commands::{commands::Parser as _, run, Args};
-use yansi::Paint as _;
+use yansi::{Condition, Paint as _};
+
+const HAVE_COLOR: Condition = Condition(|| {
+    std::env::var_os("NO_COLOR").is_none() &&
+        (Condition::CLICOLOR_LIVE)() &&
+        Condition::stdouterr_are_tty_live()
+});
 
 #[tokio::main]
 async fn main() {
+    // disable colors if unsupported
+    yansi::whenever(HAVE_COLOR);
     banner();
     let args = Args::parse();
     if let Err(err) = run(args.command).await {
